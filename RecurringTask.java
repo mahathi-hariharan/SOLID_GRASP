7a4dcda90d87dc8ce7e9733c2f2033ffb4639f72
mahathi-hariharan <mhariharan9@gatech.edu>
2025-10-24 22:32:08 -0400

    Update RecurringTask.java

--- RecurringTask.java
+++ RecurringTask.java
@@ -1,1 +1,17 @@
+import java.time.LocalDate;
+
+public class RecurringTask extends Task {
+    private int repeatIntervalDays;
+
+    public RecurringTask(String title, String description, LocalDate dueDate,
+                         TaskStatus status, TaskPriority priority,
+                         int repeatIntervalDays) {
+        super(title, description, dueDate, status, priority);
+        this.repeatIntervalDays = repeatIntervalDays;
+    }
+
+    public int getRepeatIntervalDays() {
+        return repeatIntervalDays;
+    }
+}
 

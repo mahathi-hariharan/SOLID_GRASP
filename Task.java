fe78a90b4f0a2849dbfc29791e9ef3862ec8edaa
mahathi-hariharan <mhariharan9@gatech.edu>
2025-10-24 22:33:04 -0400

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,42 @@
+import java.time.LocalDate;
 
+public abstract class Task {
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private TaskStatus status;
+    private TaskPriority priority;
+
+    public Task(String title, String description, LocalDate dueDate,
+                TaskStatus status, TaskPriority priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setStatus(TaskStatus newStatus) {
+        this.status = newStatus;
+    }
+}
